e49681ac503bc6bbff1032b805eb11cc9cd1b32f
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-25 18:51:09 +0200

    stem & rank results; weigh queries

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,8 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.db.models import Count
 from taggit.models import Tag
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, \
+    SearchQuery, SearchRank
 from .models import Post
 from .forms import EmailPostForm, CommentForm, SearchForm
 
@@ -86,8 +87,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A') + \
+                SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body')
-            ).filter(search=query)
+                rank=SearchRank(search_vector, search_query)
+            ).filter(rank__gte=0.3).order_by('-rank')
     return render(request, 'blog/post/search.html',
                   {'form': form, 'results': results, 'query': query})
